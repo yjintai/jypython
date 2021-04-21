cf71730dc0e780f76c3e73350240a06e98888310
yjintai <yjintai@126.com>
2021-04-21 11:05:54 +0800

    20210421

--- Rabbit/getdatatosql.py
+++ Rabbit/getdatatosql.py
@@ -38,11 +38,26 @@ def get_stock_basic(engine = sqlenginestr,schema = databasename):
         pass
     print('download stock_basic data successed!')
     return 1
+#Get Hu Sheng Gu Tong list
+def get_hs_const(engine = sqlenginestr,schema = databasename):
+    print('start to download hs_const data') 
+    pro = ts.pro_api()
+    dfsh = pro.hs_const(hs_type='SH', fields='ts_code,hs_type,in_date,out_date,is_new')
+    dfsz = pro.hs_const(hs_type='SZ', fields='ts_code,hs_type,in_date,out_date,is_new')
+    df = dfsh.append(dfsz,ignore_index=True)
+    try:
+        pd.io.sql.to_sql(frame=df, name='tb_hs_const', con=engine, schema= schema, if_exists='replace', index=True) 
+    except:
+        print('To SQL Database Failed')
+    finally:
+        pass
+    print('download hs_const data successed!')
+    return 1
 
 def get_trade_cal(engine = sqlenginestr,schema = databasename,start_date='20200101', end_date='20210412'):
     print('start to download trade_cal data') 
     pro = ts.pro_api()
-    df = pro.trade_cal(start_date='20200101', end_date=end_date, fields='exchange,cal_date,is_open')
+    df = pro.trade_cal(start_date=start_date, end_date=end_date, fields='exchange,cal_date,is_open')
     try:
         pd.io.sql.to_sql(frame=df, name='tb_trade_cal', con=engine, schema= schema, if_exists='replace', index=True) 
     except:
@@ -82,22 +97,97 @@ def get_daily_data(engine = sqlenginestr,schema = databasename,start_date='20210
                 print('To SQL Database Failed')
             finally:
                 pass
-        '''
-        #insert into SQL for each line in df_all, ignore duplicates
-        for indexs in df_all.index :
-            line = df_all.iloc[indexs:indexs+1]
+    return 1
+
+def get_moneyflow_hsgt(engine = sqlenginestr,schema = databasename,start_date='20210101', end_date='20210412'):
+    print('start to download moneyflow_hsgt data') 
+    pro = ts.pro_api()
+    df = pro.moneyflow_hsgt(start_date=start_date, end_date=end_date)
+    try:
+        pd.io.sql.to_sql(frame=df, name='tb_moneyflow_hsgt', con=engine, schema= schema, if_exists='replace', index=True) 
+    except:
+        print('To SQL Database Failed')
+    finally:
+        pass
+    print('download moneyflow_hsgt data successed!')
+    return 1
+
+def get_rzrq_margin(engine = sqlenginestr,schema = databasename,start_date='20210101', end_date='20210412'):
+    print('start to download rzrq_margin data') 
+    pro = ts.pro_api()
+    df = pro.margin(start_date=start_date, end_date=end_date)
+    try:
+        pd.io.sql.to_sql(frame=df, name='tb_rzrq_margin', con=engine, schema= schema, if_exists='replace', index=True) 
+    except:
+        print('To SQL Database Failed')
+    finally:
+        pass
+    print('download rzrq_margin data successed!')
+    return 1
+
+def get_daily_top_list_data(engine = sqlenginestr,schema = databasename,start_date='20210412', end_date='20210412'):
+    print('start to download daily_top_list_data data') 
+    pro = ts.pro_api()
+    fmt = '%Y%m%d'
+    begin=datetime.datetime.strptime(start_date,fmt)
+    end=datetime.datetime.strptime(end_date,fmt)
+    for i in range((end - begin).days+1):
+        date = begin + datetime.timedelta(days=i)
+        date_str = date.strftime('%Y%m%d')
+        print(date_str)
+        df_top_list = pro.top_list(trade_date=date_str)  
+        df_top_inst = pro.top_inst(trade_date=date_str)        
+        if df_top_list.empty:
+            print ('Empty data in ' + date_str)
+        else:
             try:
-                pd.io.sql.to_sql(frame=line, name='tb_daily_data', con=engine, schema= schema, if_exists='append', index=False, chunksize=1)
+                pd.io.sql.to_sql(frame=df_top_list, name='tb_daily_top_list', con=engine, schema= schema, if_exists='append', index=False) 
+                pd.io.sql.to_sql(frame=df_top_inst, name='tb_daily_top_inst', con=engine, schema= schema, if_exists='append', index=False) 
+                print('download ' +date_str+ ' daily_top_list_data successed!')
             except exc.IntegrityError:
-                #Ignore duplicates
-                print ("duplicated line")
-                pass
+                    #Ignore duplicates
+                    print ("duplicated data " + date_str)
+                    pass
             except:
                 print('To SQL Database Failed')
             finally:
                 pass
-        '''
+    return 1
 
+def get_concept(engine = sqlenginestr,schema = databasename):
+    print('start to download concept data') 
+    pro = ts.pro_api()
+    try:
+        df_concept = pro.concept()
+        pd.io.sql.to_sql(frame=df_concept, name='tb_concept', con=engine, schema= schema, if_exists='replace', index=True) 
+    except:
+        print('To SQL Database Failed')
+    finally:
+        pass
+    print('download concept data successed!')
+
+    for i in range(0, len(df_concept)): 
+        code = df_concept.iloc[i]['code']
+        if i == 0:
+            ifexist = 'replace'
+        else:
+            ifexist = 'append'
+        for retry in range(3):
+            try:
+                df_concept_detail = pro.concept_detail(id = code)
+                print ('Got concept:'+code)
+                time.sleep(0.4)
+                pd.io.sql.to_sql(frame=df_concept_detail, name='tb_concept_detail', con=engine, schema = schema, if_exists = ifexist, index=True) 
+                print ('To SQL:'+code)
+                break
+            except:
+                if retry < 2:
+                    print('failed. retry...')
+                else:
+                    print('failed.')
+            finally:
+                pass
+    print('download concept data detail successed!')
     return 1
 
 #全量下载所有股票列表数据
@@ -105,19 +195,31 @@ if __name__ == '__main__':
     print('开始')
     engine = initiate()
     end_date = datetime.datetime.now().strftime('%Y%m%d')
-    start_date = '20201001'
-    end_date = '20210101'
-    
+    start=datetime.datetime.now() -datetime.timedelta(days = 1)
+    start_date = start.strftime('%Y%m%d')
     
     print('获取列表...')
     #get stock basic info
     #get_stock_basic(engine,databasename)
 
-    #get stock trade calender from start_date to end_date
+    #get stock basic info
+    #get_hs_const(engine,databasename)
 
-    #get_trade_cal(engine,databasename,start_date,end_date)
+    #get stock trade calender from start_date to end_date
+    #get_trade_cal(engine,databasename,'20200101',end_date)
+
+    #get stock concept info
+    #get_concept(engine,databasename)
+
+    #get HSGT money flow data from start_date to end_date
+    get_moneyflow_hsgt(engine,databasename,'20200101',end_date)
+    
+    #get RZRQ margin data from start_date to end_date
+    get_rzrq_margin(engine,databasename,'20200101',end_date)
 
     #get stock daily data from start_date to end_date
     get_daily_data(engine,databasename,start_date,end_date)
+    #get_daily_top_list_data from start_date to end_date
+    get_daily_top_list_data(engine,databasename,start_date,end_date)
 
     print('结束')
