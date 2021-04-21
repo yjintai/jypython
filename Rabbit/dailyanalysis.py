aa0096bd538c6f14fd429f290bb581e9e9e20374
yjintai <yjintai@126.com>
2021-04-21 18:38:26 +0800

    20210421

--- Rabbit/dailyanalysis.py
+++ Rabbit/dailyanalysis.py
@@ -1,20 +1,24 @@
+import time
+import datetime
 import pandas as pd
 import requests
 import os
-import time
 import warnings
 
 import sqlalchemy
 from sqlalchemy import exc
 import pymysql
 
-stock_list_file = 'd:/stock_list.csv'
 databasename = 'msstock'
 sqlenginestr='mysql+pymysql://root:root@127.0.0.1/'+databasename+'?charset=utf8mb4'
 
 warnings.filterwarnings('ignore')
 pd.set_option('expand_frame_repr', False)
-basedir = 'D:/Works/python/test'
+basedir = 'D:/Works/python/jypython/rabbit'
+
+def initiate():
+    engine=sqlalchemy.create_engine(sqlenginestr)
+    return engine
 
 def myprint (str,filename, mode = 'a'):
     print (str)
@@ -31,69 +35,102 @@ def get_group_output(df, columns='industry', name='_limit_up'):
     output.rename(columns={'ts_code': 'count'}, inplace=True)
     return output
 
-# 获取指定列的分析统计结果
+# 获取指定日期的分析统计结果
 def daily_analysis (date):
     date_now = date
     analysisfilename = basedir+'/dailyanalysis/'+ str(date) + '_report.csv'
     
     myprint(str(date_now),analysisfilename, 'wt')
+    engine = sqlalchemy.create_engine(sqlenginestr)
+    
+    # 读取指数信息
+    sql = '''SELECT tb_index_daily.*,
+    tb_index_dailybasic.pe,
+    tb_index_dailybasic.pb
+    FROM msstock.tb_index_daily
+    left join msstock.tb_index_dailybasic 
+    on (tb_index_daily.ts_code = tb_index_dailybasic.ts_code and tb_index_daily.trade_date = tb_index_dailybasic.trade_date) 
+    where tb_index_daily.trade_date = "%s"
+    order by tb_index_daily.ts_code;'''%date_now
+    df_index = pd.read_sql_query(sql, engine)
+    df_index.fillna(0, inplace=True)
+    df_index.replace('nan ', 0, inplace=True)
+    myprint('Index Data：',analysisfilename)
+    df_index.to_csv(analysisfilename, index=False, encoding='utf_8_sig',mode = 'w', float_format = '%.2f')
+
     # 读取TDX的每日数据
-    df = pd.read_csv(basedir + '/input_data/{}_ts.csv'.format(str(date_now)), encoding='utf_8_sig')
+    sql = '''SELECT tb_daily_data.ts_code,
+            tb_daily_data.trade_date,
+            tb_stock_basic.name,
+            tb_stock_basic.industry, 
+            tb_stock_basic.market,
+            tb_daily_data.open,
+            tb_daily_data.close,
+            tb_daily_data.pre_close,
+            tb_daily_data.change,
+            tb_daily_data.pct_chg,
+            tb_daily_data.turnover_rate,
+            tb_daily_data.volume_ratio,
+            tb_daily_data.pe,
+            tb_daily_data.pb,
+            tb_daily_data.amount,
+            tb_stock_basic.list_date
+            FROM msstock.tb_daily_data 
+            left join msstock.tb_stock_basic 
+            on (tb_daily_data.ts_code = tb_stock_basic.ts_code) 
+            where tb_daily_data.trade_date = "%s" 
+            order by tb_daily_data.pct_chg desc;''' %date_now
+    df = pd.read_sql_query(sql, engine)
     df.fillna(0, inplace=True)
     df.replace('nan ', 0, inplace=True)
 
-    df_up = df[df['pct_chg'].astype(float) > 0.00]
-    df_down = df[df['pct_chg'].astype(float)< 0.00]
+    df_up = df[df['pct_chg'] > 0.00]
+    df_down = df[df['pct_chg'] < 0.00]
     df_limit_up = df[(((df['market']=='创业板') | (df['market']=='科创板')) & (df['pct_chg'] >= 19.7)) | (((df['market']!='创业板') & (df['market']!='科创板')) & (df['pct_chg'] >= 9.7))]
-    df_limit_up_new = df_limit_up[pd.to_datetime(pd.to_datetime(df_limit_up['trade_date_x'])) - pd.to_datetime(pd.to_datetime(df_limit_up['list_date'])) <= pd.Timedelta(30)]
+    df_limit_up_new = df_limit_up[pd.to_datetime(date_now) - pd.to_datetime(df_limit_up['list_date']) <= pd.Timedelta(365)]
 
     df_main = df[((df['market']!='创业板') & (df['market']!='科创板'))]
     df_up_main = df_up[((df_up['market']!='创业板') & (df_up['market']!='科创板'))]
     df_down_main = df_down[((df_down['market']!='创业板') & (df_down['market']!='科创板'))]
     df_limit_up_main = df_up_main[(df_up_main['pct_chg'] >= 9.7)]
-    df_limit_up_new_main = df_limit_up_main[pd.to_datetime(pd.to_datetime(df_limit_up_main['trade_date_x'])) - pd.to_datetime(pd.to_datetime(df_limit_up_main['list_date'])) <= pd.Timedelta(30)]
+    df_limit_up_new_main = df_limit_up_main[pd.to_datetime(date_now) - pd.to_datetime(df_limit_up['list_date']) <= pd.Timedelta(365)]
 
     df_chuangye = df[(df['market']=='创业板')]
     df_up_chuangye = df_up[(df_up['market']=='创业板')]
     df_down_chuangye = df_down[(df_down['market']=='创业板')]
     df_limit_up_chuangye = df_up_chuangye[(df_up_chuangye['pct_chg'] >= 19.7)]
-    df_limit_up_new_chuangye= df_limit_up_chuangye[pd.to_datetime(pd.to_datetime(df_limit_up_chuangye['trade_date_x'])) - pd.to_datetime(pd.to_datetime(df_limit_up_chuangye['list_date'])) <= pd.Timedelta(30)]
+    df_limit_up_new_chuangye= df_limit_up_chuangye[pd.to_datetime(date_now) - pd.to_datetime(df_limit_up['list_date']) <= pd.Timedelta(365)]
 
     df_kechuang = df[(df['market']=='科创板')]
     df_up_kechuang = df_up[(df_up['market']=='科创板')]
     df_down_kechuang = df_down[(df_down['market']=='科创板')]
     df_limit_up_kechuang = df_up_kechuang[(df_up_kechuang['pct_chg'] >= 19.7)]
-    df_limit_up_new_kechuang= df_limit_up_kechuang[pd.to_datetime(pd.to_datetime(df_limit_up_kechuang['trade_date_x'])) - pd.to_datetime(pd.to_datetime(df_limit_up_kechuang['list_date'])) <= pd.Timedelta(30)]
-
-
+    df_limit_up_new_kechuang= df_limit_up_kechuang[pd.to_datetime(date_now) - pd.to_datetime(df_limit_up['list_date']) <= pd.Timedelta(365)]
 
     df_output_general = pd.DataFrame(columns=('market','up', 'down', 'amount', 'limit up', 'new limit up'))
-    df_output_general.loc[1] = ['All',df_up.shape[0],df_down.shape[0],df['amount'].sum()/10,df_limit_up.shape[0],df_limit_up_new.shape[0]]
-    df_output_general.loc[2] = ['ZhuBan',df_up_main.shape[0],df_down_main.shape[0],df_main['amount'].sum()/10,df_limit_up_main.shape[0],df_limit_up_new_main.shape[0]]
-    df_output_general.loc[3] = ['ChuangYe',df_up_chuangye.shape[0],df_down_chuangye.shape[0],df_chuangye['amount'].sum()/10,df_limit_up_chuangye.shape[0],df_limit_up_new_chuangye.shape[0]]
-    df_output_general.loc[4] = ['KeChuang',df_up_kechuang.shape[0],df_down_kechuang.shape[0],df_kechuang['amount'].sum()/10,df_limit_up_kechuang.shape[0],df_limit_up_new_kechuang.shape[0]]
-
+    df_output_general.loc[1] = ['全部',df_up.shape[0],df_down.shape[0],df['amount'].sum()/10,df_limit_up.shape[0],df_limit_up_new.shape[0]]
+    df_output_general.loc[2] = ['主板',df_up_main.shape[0],df_down_main.shape[0],df_main['amount'].sum()/10,df_limit_up_main.shape[0],df_limit_up_new_main.shape[0]]
+    df_output_general.loc[3] = ['创业板',df_up_chuangye.shape[0],df_down_chuangye.shape[0],df_chuangye['amount'].sum()/10,df_limit_up_chuangye.shape[0],df_limit_up_new_chuangye.shape[0]]
+    df_output_general.loc[4] = ['科创板',df_up_kechuang.shape[0],df_down_kechuang.shape[0],df_kechuang['amount'].sum()/10,df_limit_up_kechuang.shape[0],df_limit_up_new_kechuang.shape[0]]
     df_output_general[['up', 'down', 'amount', 'limit up', 'new limit up']] = df_output_general[['up', 'down', 'amount', 'limit up', 'new limit up']].astype(int)
-    myprint('General Data 盘面：',analysisfilename)
-    df_output_general.to_csv(analysisfilename, index=False, encoding='utf_8_sig',mode = 'w', float_format = '%.2f')
     
 
+    
+    myprint('General Data:',analysisfilename)
+    df_output_general.to_csv(analysisfilename, index=False, encoding='utf_8_sig',mode = 'a', float_format = '%.2f')
+    
     myprint('Limit up:',analysisfilename)
-    df_limit_up.sort_values('pct_chg', ascending=False, inplace=True)
-    df_limit_up_log = df_limit_up.loc[:,['ts_code','name','close_x','pct_chg','turnover_rate','volume_ratio','pe','total_mv','circ_mv','area','industry','market','list_date']]
-    #df_limit_up_log = df_limit_up.loc[:,['symbol','name','close_x','pct_chg']]
-    df_limit_up_log.rename(columns={'close_x': 'close'}, inplace=True)
-    #df_limit_up_log['symbol'] = df['symbol'].astype(str)
 
-    df_limit_up_log.to_csv(analysisfilename, index=False, encoding='utf_8_sig',mode = 'a', float_format = '%.2f')
+    df_limit_up.to_csv(analysisfilename, index=False, encoding='utf_8_sig',mode = 'a', float_format = '%.2f')
 
     myprint('Limit up Group:',analysisfilename)
     for i in ['industry']:
         output_limit_up = get_group_output(df_limit_up, columns=i, name='limit_up')
         output_limit_up.to_csv(analysisfilename, index=True, encoding='utf_8_sig',mode = 'a', float_format = '%.2f')
-    
+
 if __name__ == '__main__':
     print('start...')
     print('analyze daily data')
-    daily_analysis(date='20210414')
+    date = datetime.datetime.now().strftime('%Y%m%d')
+    daily_analysis(date=date)
     print('end')
--- Rabbit/getdatatosql.py
+++ Rabbit/getdatatosql.py
@@ -12,7 +12,6 @@ import sqlalchemy
 from sqlalchemy import exc
 import pymysql
 
-stock_list_file = 'd:/stock_list.csv'
 databasename = 'msstock'
 sqlenginestr='mysql+pymysql://root:root@127.0.0.1/'+databasename+'?charset=utf8mb4'
 
@@ -77,26 +76,38 @@ def get_daily_data(engine = sqlenginestr,schema = databasename,start_date='20210
         date = begin + datetime.timedelta(days=i)
         date_str = date.strftime('%Y%m%d')
         print(date_str)
-        df_daily = pro.daily(trade_date=date_str,fields='ts_code,trade_date,open,high,low,close,pre_close,change,pct_chg,vol,amount')
-        df_daily_adjfactor = pro.adj_factor(trade_date=date_str, fields='ts_code,adj_factor')
-        df_daily_basic = pro.daily_basic(trade_date=date_str,fields='ts_code,turnover_rate,turnover_rate_f,volume_ratio,pe,pe_ttm,pb,ps,ps_ttm,dv_ratio,dv_ttm,total_share,float_share,free_share,total_mv,circ_mv')
-        df_all = pd.merge(left=df_daily, right=df_daily_adjfactor, on='ts_code', how='left')
-        df_all = pd.merge(left=df_all, right=df_daily_basic, on='ts_code', how='left')
         
-        if df_all.empty:
-            print ('Empty data in ' + date_str)
-        else:
+        for retry in range(3):
             try:
-                pd.io.sql.to_sql(frame=df_all, name='tb_daily_data', con=engine, schema= schema, if_exists='append', index=False) 
-                print('download ' +date_str+ ' daily data successed!')
-            except exc.IntegrityError:
-                    #Ignore duplicates
-                    print ("duplicated data " + date_str)
-                    pass
+                df_daily = pro.daily(trade_date=date_str,fields='ts_code,trade_date,open,high,low,close,pre_close,change,pct_chg,vol,amount')
+                df_daily_adjfactor = pro.adj_factor(trade_date=date_str, fields='ts_code,adj_factor')
+                df_daily_basic = pro.daily_basic(trade_date=date_str,fields='ts_code,turnover_rate,turnover_rate_f,volume_ratio,pe,pe_ttm,pb,ps,ps_ttm,dv_ratio,dv_ttm,total_share,float_share,free_share,total_mv,circ_mv')
+                df_all = pd.merge(left=df_daily, right=df_daily_adjfactor, on='ts_code', how='left')
+                df_all = pd.merge(left=df_all, right=df_daily_basic, on='ts_code', how='left')
+                if df_all.empty:
+                    print ('Empty data in ' + date_str)
+                else:
+                    try:
+                        pd.io.sql.to_sql(frame=df_all, name='tb_daily_data', con=engine, schema= schema, if_exists='append', index=False) 
+                        print('download ' +date_str+ ' daily data successed!')
+                    except exc.IntegrityError:
+                            #Ignore duplicates
+                            print ("duplicated data " + date_str)
+                            pass
+                    except:
+                        print('To SQL Database Failed')
+                    finally:
+                        pass
+                break
             except:
-                print('To SQL Database Failed')
+                if retry < 2:
+                    print('failed. retry...')
+                else:
+                    print('failed.')
             finally:
                 pass
+        
+        
     return 1
 
 def get_moneyflow_hsgt(engine = sqlenginestr,schema = databasename,start_date='20210101', end_date='20210412'):
@@ -190,36 +201,111 @@ def get_concept(engine = sqlenginestr,schema = databasename):
     print('download concept data detail successed!')
     return 1
 
+def get_index_basic(engine = sqlenginestr,schema = databasename):
+    print('start to download index_basic data') 
+    pro = ts.pro_api()
+    df = pro.index_basic()
+    try:
+        pd.io.sql.to_sql(frame=df, name='tb_index_basic', con=engine, schema= schema, if_exists='replace', index=True) 
+    except:
+        print('To SQL Database Failed')
+    finally:
+        pass
+    print('download index_basic data successed!')
+    return 1
+
+def get_index_daily(engine = sqlenginestr,schema = databasename,start_date='20210412', end_date='20210412'):
+    print('start to download index_daily')  
+    ls = ['000001.SH','000005.SH','000300.SH','000905.SH','399001.SZ','399006.SZ']
+    pro = ts.pro_api()  
+    fmt = '%Y%m%d'
+    begin=datetime.datetime.strptime(start_date,fmt)
+    end=datetime.datetime.strptime(end_date,fmt)
+    for i in range((end - begin).days+1):
+        date = begin + datetime.timedelta(days=i)
+        date_str = date.strftime('%Y%m%d')
+        print(date_str)  
+        for tscode in ls:
+            print(tscode)
+            try:
+                dfid = pro.index_daily(ts_code=tscode,trade_date=date_str)
+                if dfid.empty:
+                    print ('Empty data in ' + date_str)
+                else:
+                    try:
+                        pd.io.sql.to_sql(frame=dfid, name='tb_index_daily', con=engine, schema= schema, if_exists='append', index=False) 
+                        print('download ' +date_str+ ' index_daily data successed!')
+                    except exc.IntegrityError:
+                            print ("duplicated data " + date_str)
+                            pass
+                    except:
+                        print('To SQL Database Failed')
+                    finally:
+                        pass
+            except:
+                print('From Tushare failed.')
+            finally:
+                pass           
+    return 1
+def get_index_dailybasic(engine = sqlenginestr,schema = databasename,start_date='20210412', end_date='20210412'):
+    print('start to download index_dailybasic')  
+    ls = ['000001.SH','000005.SH','000300.SH','000905.SH','399001.SZ','399006.SZ']
+    pro = ts.pro_api()  
+    fmt = '%Y%m%d'
+    begin=datetime.datetime.strptime(start_date,fmt)
+    end=datetime.datetime.strptime(end_date,fmt)
+    for i in range((end - begin).days+1):
+        date = begin + datetime.timedelta(days=i)
+        date_str = date.strftime('%Y%m%d')
+        print(date_str)  
+        for tscode in ls:
+            print(tscode)
+            try:
+                dfidb = pro.index_dailybasic(ts_code=tscode,trade_date=date_str)
+                if dfidb.empty:
+                    print ('Empty data in ' + date_str)
+                else:
+                    try:
+                        pd.io.sql.to_sql(frame=dfidb, name='tb_index_dailybasic', con=engine, schema= schema, if_exists='append', index=False)           
+                        print('download ' +date_str+ ' index_dailybasic data successed!')
+                    except exc.IntegrityError:
+                            print ("duplicated data " + date_str)
+                    except:
+                        print('To SQL Database Failed')
+                    finally:
+                        pass
+            except:
+                print('From Tushare failed.')
+            finally:
+                pass           
+    return 1
+
 #全量下载所有股票列表数据
 if __name__ == '__main__':
     print('开始')
     engine = initiate()
     end_date = datetime.datetime.now().strftime('%Y%m%d')
-    start=datetime.datetime.now() -datetime.timedelta(days = 1)
+    start=datetime.datetime.now() -datetime.timedelta(days = 0)
     start_date = start.strftime('%Y%m%d')
+    #start_date = '20100101'
     
     print('获取列表...')
-    #get stock basic info
-    #get_stock_basic(engine,databasename)
-
-    #get stock basic info
+ 
+    #May be updated weekly.
     #get_hs_const(engine,databasename)
-
-    #get stock trade calender from start_date to end_date
     #get_trade_cal(engine,databasename,'20200101',end_date)
-
-    #get stock concept info
     #get_concept(engine,databasename)
+    #get_index_basic(engine,databasename)
 
-    #get HSGT money flow data from start_date to end_date
-    get_moneyflow_hsgt(engine,databasename,'20200101',end_date)
+    #updated after 24:00AM every day 
+    #get_moneyflow_hsgt(engine,databasename,'20200101',end_date)
+    #get_rzrq_margin(engine,databasename,'20200101',end_date)
+    #get_daily_top_list_data(engine,databasename,start_date,end_date)
     
-    #get RZRQ margin data from start_date to end_date
-    get_rzrq_margin(engine,databasename,'20200101',end_date)
-
-    #get stock daily data from start_date to end_date
-    get_daily_data(engine,databasename,start_date,end_date)
-    #get_daily_top_list_data from start_date to end_date
-    get_daily_top_list_data(engine,databasename,start_date,end_date)
-
+    #updated after 3:00PM every day
+    #get_stock_basic(engine,databasename)
+    #get_daily_data(engine,databasename,start_date,end_date)
+    get_index_daily(engine,databasename,start_date,end_date)
+    get_index_dailybasic(engine,databasename,start_date,end_date)
+    
     print('结束')
